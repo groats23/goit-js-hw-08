870ff140d1b8021038f63d98bed8ea9f1a4a2d02
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-11 21:41:35 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(currentTimeSaveHandler, 1000));
+
+function currentTimeSaveHandler(time) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(time));
+}
+
+const currentTimeInSeconds = JSON.parse(
+  localStorage.getItem(STORAGE_KEY)
+).seconds;
+
+player.setCurrentTime(currentTimeInSeconds).catch(function (error) {
+  console.log(error.name);
+  console.log(error.message);
+});
