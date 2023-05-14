b0f715e390158200096e19f5e0db3c962b20de46
groats23 <121118901+groats23@users.noreply.github.com>
2023-05-14 10:23:26 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  feedbackForm: document.querySelector('.feedback-form'),
+  formInput: document.querySelector('.feedback-form input'),
+  formTextarea: document.querySelector('.feedback-form textarea'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formFeedbackData = {};
+const userFormData = {
+  email: refs.formInput,
+  message: refs.formTextarea,
+};
+
+refs.feedbackForm.addEventListener('input', throttle(formInputHandler, 500));
+refs.feedbackForm.addEventListener('submit', onSubmitFormBtnClick);
+populateSavedData();
+
+function formInputHandler(e) {
+  formFeedbackData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formFeedbackData));
+}
+
+function populateSavedData() {
+  const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedFormData.email) {
+    userFormData.email.value = savedFormData.email;
+    formFeedbackData.email = userFormData.email.value;
+  }
+  if (savedFormData.message) {
+    userFormData.message.value = savedFormData.message;
+    formFeedbackData.message = userFormData.message.value;
+  }
+}
+
+function onSubmitFormBtnClick(e) {
+  e.preventDefault();
+  if (
+    e.currentTarget.email.value === '' ||
+    e.currentTarget.message.value === ''
+  ) {
+    alert('Please, fill in empty areas in order to submit form!');
+  } else {
+    console.log('formData: ', formFeedbackData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
